c76e67fe313503526cf077aeeb9ec3b4414d51d4
Ivan Filho <i@ivanfilho.com>
2022-02-07 20:30:42 -0300

    refactor properties panel and fix onchange bug

---
+++ src/components/PropertiesPanel/Field.tsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import useSelection from '../../util/useSelection'
+import styles from './styles.module.css'
+
+export default function Field ({ name, type }: any) {
+  const { component, setComponent } = useSelection()
+  const value = component?.config && component.config[name]
+
+  const handleChange = (value: any) => {
+    setComponent({
+      ...component,
+      config: {
+        ...(component.config || {}),
+        [name]: value
+      }
+    })
+  }
+
+  const renderInput = () => {
+    const commonProps: any = {
+      name,
+      value: value || '',
+      onChange: (event: any) => handleChange(event.target.value)
+    }
+
+    if (Array.isArray(type)) {
+      return (
+        <select {...commonProps}>
+          {type.map(option => (
+            <option key={option}>{option}</option>
+          ))}
+        </select>
+      )
+    }
+
+    if (type === 'number') {
+      return <input type='number' {...commonProps} />
+    }
+
+    if (type === 'boolean') {
+      return (
+        <input
+          type='checkbox'
+          name={name}
+          checked={!!value}
+          onChange={(event: any) => handleChange(event.target.checked)}
+        />
+      )
+    }
+
+    return <input type='text' {...commonProps} />
+  }
+
+  return (
+    <div className={styles.field}>
+      <label htmlFor={name}>{name}</label>
+      {renderInput()}
+    </div>
+  )
+}
--- src/components/PropertiesPanel/index.tsx
+++ src/components/PropertiesPanel/index.tsx
@@ -2,67 +2,16 @@ import React from 'react'
 import useComponent from '../../util/useComponent'
 import usePlugins from '../../util/usePlugins'
 import useSelection from '../../util/useSelection'
+import Field from './Field'
 import styles from './styles.module.css'
 
 export default function PropertiesPanel () {
-  const { component, setComponent } = useSelection()
+  const { component } = useSelection()
   const { config, description, plugins: pluginNames = [] } = useComponent(
     component?.type
   )
   const plugins = usePlugins(pluginNames)
   const configTypes: any = config || {}
-
-  const handleChange = (field: string, value: any) => {
-    setComponent({
-      ...component,
-      config: {
-        ...(component.config || {}),
-        [field]: value
-      }
-    })
-  }
-
-  const renderInput = (field: string, type: any) => {
-    const commonProps: any = {
-      name: field,
-      value: (component?.config && component.config[field]) || '',
-      onChange: (event: any) => handleChange(field, event.target.value)
-    }
-
-    if (Array.isArray(type)) {
-      return (
-        <select {...commonProps}>
-          {type.map(option => (
-            <option key={option}>{option}</option>
-          ))}
-        </select>
-      )
-    }
-
-    if (type === 'number') {
-      return <input type='number' {...commonProps} />
-    }
-
-    if (type === 'boolean') {
-      return (
-        <input
-          type='checkbox'
-          name={field}
-          checked={!!(component?.config && component.config[field])}
-          onChange={(event: any) => handleChange(field, event.target.checked)}
-        />
-      )
-    }
-
-    return <input type='text' {...commonProps} />
-  }
-
-  const Field = ({ field, type }: any) => (
-    <div className={styles.field}>
-      <label htmlFor={field}>{field}</label>
-      {renderInput(field, type)}
-    </div>
-  )
 
   return (
     <div className={styles.propertiesPanel}>
@@ -71,13 +20,13 @@ export default function PropertiesPanel () {
       <hr />
       <div className={styles.form}>
         {Object.keys(configTypes).map(field => (
-          <Field key={field} field={field} type={configTypes[field]} />
+          <Field key={field} name={field} type={configTypes[field]} />
         ))}
         {plugins.map((plugin, index) => (
           <React.Fragment key={plugin}>
             <h3>{pluginNames[index]}</h3>
             {Object.keys(plugin.config || {}).map(field => (
-              <Field key={field} field={field} type={plugin.config[field]} />
+              <Field key={field} name={field} type={plugin.config[field]} />
             ))}
           </React.Fragment>
         ))}
